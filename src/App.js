8c70f65936bd3c9703b5b4dd0b770bd0850d8098
stevecyj <ycsteven.tsao@gmail.com>
2022-12-14 06:11:26 +0800

    chore: hello semi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Toast } from '@douyinfe/semi-ui';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Button onClick={() => Toast.warning({ content: 'welcome' })}>
+      Hello Semi
+    </Button>
   );
 }
 
