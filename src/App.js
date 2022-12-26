e35984bde466cd7afcd975e5461432c353ddcb7b
Steve <ycsteven.tsao@gmail.com>
2022-12-27 06:56:11 +0800

    feat: route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import MarketplacePage from './pages/MarketplacePage';
+import NftPage from "@/pages/NftPage/NftPage";
+import MarketplacePage from "@/pages/MarketplacePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NftPage />,
+  },
+  {
+    path: "/marketplace",
+    element: <MarketplacePage />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <MarketplacePage></MarketplacePage>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
